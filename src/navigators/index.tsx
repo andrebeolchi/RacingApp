a20f9e8b92ca3ccca530b49b1b17fe8ebec736ae
andrebeolchi <rm84163@fiap.com.br>
2022-09-15 14:46:19 -0300

    Navigation to Edit Page

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -1,21 +1,47 @@
 import { NavigationContainer } from "@react-navigation/native";
+import type { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import Button from "../components/Button";
+import { useTheme } from "../hooks/useTheme";
 import Edit from "../screens/Edit";
 import Home from "../screens/Home";
 
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigator() {
+    const { theme } = useTheme();
+
+    const screenOptions: NativeStackNavigationOptions = {
+        headerStyle: {
+            backgroundColor: theme.surface,
+        },
+        headerTitleStyle: {
+            color: theme.text.primary,
+        },
+        headerShadowVisible: true,
+        headerTintColor: theme.text.primary,
+        animation: "fade_from_bottom",
+    };
+
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName={"Home"}>
+            <Stack.Navigator
+                initialRouteName={"Home"}
+                screenOptions={screenOptions}
+            >
                 <Stack.Screen
                     name={"Home"}
                     component={Home}
-                    options={{
+                    options={({ navigation }) => ({
                         title: "Resultados",
-                    }}
+                        headerRight: () => (
+                            <Button
+                                label="Editar"
+                                onPress={() => navigation.navigate("Edit")}
+                            />
+                        ),
+                    })}
                 />
                 <Stack.Screen
                     name={"Edit"}
